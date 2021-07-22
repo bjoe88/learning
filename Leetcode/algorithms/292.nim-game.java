6149fccb6d87b2145c2ce7b01e23545acd01301a
Muhammad Azizol <bjoe88@gmail.com>
2021-07-22 22:24:52 +0200

    Add solution for 292

---
+++ Leetcode/algorithms/292.nim-game.java
@@ -0,0 +1,20 @@
+package Leetcode.algorithms;
+/*
+ * @lc app=leetcode id=292 lang=java
+ *
+ * [292] Nim Game
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
+// @lc code=end
+/*
+ * 60/60 cases passed (0 ms)
+ * Your runtime beats 100 % of java submissions
+ * Your memory usage beats 98.71 % of java submissions (35.3 MB)
+ */
+
