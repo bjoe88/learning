302cc153db39a6fd100766464bcb71d40751b41d
Muhammad Azizol <bjoe88@gmail.com>
2021-07-23 22:14:08 +0200

    Refactor 349

--- Leetcode/algorithms/349.intersection-of-two-arrays.java
+++ Leetcode/algorithms/349.intersection-of-two-arrays.java
@@ -12,19 +12,28 @@ import java.util.List;
 // @lc code=start
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-      HashSet<Integer> set1 = new HashSet<Integer>();
-      for (Integer n : nums1) set1.add(n);
-      HashSet<Integer> set2 = new HashSet<Integer>();
-      for (Integer n : nums2) set2.add(n);
-  
-      set1.retainAll(set2);
-  
-      int [] output = new int[set1.size()];
-      int idx = 0;
-      for (int s : set1) output[idx++] = s;
-      return output;
+        if (nums1.length < nums2.length)
+            return intersection(nums2, nums1);
+
+        HashSet<Integer> nums2Int = new HashSet<Integer>();
+        List<Integer> answer = new ArrayList<Integer>();
+        for (int i = 0; i < nums2.length; i++) {
+            nums2Int.add(nums2[i]);
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            if (nums2Int.contains(nums1[i])) {
+                answer.add(nums1[i]);
+                nums2Int.remove(nums1[i]);
+            }
+        }
+        int[] ans = new int[answer.size()];
+        for (int i = 0; i < answer.size(); i++) {
+            ans[i] = answer.get(i);
+        }
+        return ans;
     }
-  }
+}
 // @lc code=end
 /*
  * 55/55 cases passed (2 ms)
